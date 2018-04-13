d1a3a9992c454c95f22b309dcfdb834202485f92
Bruno augusto <brunoaugustosilva8@gmail.com>
2018-04-13 20:54:04 -0300

    Improve design

--- gulpfile.js
+++ gulpfile.js
@@ -8,35 +8,61 @@ const cache = require('gulp-cache');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const nano = require('cssnano');
+
+const plumber = require('gulp-plumber');
+
 const cssComb = require('csscomb');
 let comb = new cssComb('csscomb');
-comb.processPath('src/sass');
+comb.processPath('src/css');
+
+/* doc */
+
+const sassdoc = require('sassdoc');
 
 /* server */
 const browserSync = require('browser-sync').create();
 
+/* notications */
+
+const notify = require('gulp-notify');
+
 /*tasks*/
 
 gulp.task('sass', function() {
-    gulp.src('./src/sass/*.scss')
-        .pipe(cache(sass()))
+    return gulp.src("./src/sass/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on('error', function (err) {
+            console.log(err.toString());
+            this.emit('end');
+        })
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./src/css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./src/css"))
         .pipe(browserSync.reload({stream: true}));
 });
 
+gulp.task('sass-doc', function () {
+    return gulp.src('./src/sass/*.scss')
+        .pipe(sassdoc({dest: './src/sassdoc'}))
+});
+
 gulp.task('browser-sync', function(){
-    browserSync.init({
+    browserSync.init(["./src/css", "./src/js"],{
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch('/src/sass/*.scss', ['sass']);
-    gulp.watch('*.html').on('change', browserSync.reload);
 });
 
-
-gulp.task('server:watch', ['sass', 'browser-sync']);
+gulp.task('server:watch', ['sass', 'sass-doc', 'browser-sync'], function () {
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch([
+        './src/sass/*.scss',
+        './src/sass/*/*.scss'],
+        ['sass']);
+});
